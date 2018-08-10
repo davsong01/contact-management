c0ccdc72b263416fb39a24815a2e09b7f73fb065
David <davsong01@gmail>
2018-08-10 11:36:26 +0100

    first commit

---
+++ functions.php
@@ -0,0 +1,95 @@
+<?php
+$phoneBook = json_decode(file_get_contents('phonebook.json'));
+
+Function addContact($a, $b, $mail, $bio){
+	global $phoneBook; 
+	$success = true;
+	$contact = [$a, $b, $mail, $bio];
+	array_push($phoneBook, $contact);
+	$response = [
+		"success"=>$success,
+		"message" => ["Contact succefully added"],
+		"results"=>[],
+	];
+	return $response;
+}
+
+function viewContact(){
+	global $phoneBook;
+	$success = true;
+	$response = [
+		"success"=>$success,
+		"results"=>$phoneBook,
+		"message"=>["Found ".count($phoneBook)." result(s)"],
+	];
+	return $response;
+}
+
+function deleteContact($d){
+	global $phoneBook;
+	array_splice($phoneBook, $d, 1);
+}
+
+function deleteAll(){
+	global $phoneBook;
+	$phoneBook = [];
+	$success = true;
+	file_put_contents('phonebook.json',json_encode($phoneBook));
+	$response = [
+		"success"=>$success,
+		"results"=>[],
+		"message"=>'PhoneBook has been succesfully erased',
+	];
+return $response;	
+}
+
+function searchContact($n){
+	global $phoneBook;
+	$success = true;
+	$contactIndexarray=[];
+	$i=0;
+	foreach($phoneBook as $contact){
+		if(in_array($n, $contact, TRUE)){
+			$temp = [$i];
+			foreach($phoneBook[$i] as $item){
+				$temp[] = $item;
+			}
+			array_push($contactIndexarray, $temp);
+		}
+		$i++;
+		$response = [
+			"success"=>$success,
+			"results"=>$contactIndexarray,
+			"message"=>["Found ".count($contactIndexarray)." result(s)"],
+		];
+		return $response;
+	}}
+	function savePhonebook(){
+		global $phoneBook;
+		file_put_contents('phonebook.json',json_encode($phoneBook));
+	}
+function editContact($id, $newName='', $Phone, $mail='', $bio=''){
+	global $phoneBook;
+	$contact = $phoneBook[$id];
+	$success = true;
+	if($newName==''){
+		$newName = $contact[0];
+	if($Phone==''){
+		$newPhone = $contact[1];
+	}
+	if($mail==''){
+		$mail = $contact[2];
+	}
+	if($bio==''){
+		$bio = $contact[3];
+	}
+	$phoneBook[$id] = [$newName, $Phone, $mail, $bio];
+	$response = [
+		"success"=>$success,
+		"results"=>$phoneBook[$id],
+		"message"=>['Contact Updated']
+	];
+	return $response;
+}
+	}
+?>
---
+++ index.php
@@ -0,0 +1,83 @@
+<?php
+include 'functions.php';
+if(empty($_GET['action'])){
+    $message[] = 'SUCESS: False';
+    $message[]= 'Sorry, you did not define an action to perform';
+    echo json_encode($message);
+    
+}
+else{
+
+//Done
+$action = $_GET['action'];
+switch ($action) {
+    case 'add':
+    if(empty($_POST['phone']) || empty($_POST['name']) ||is_numeric($_POST['name']) || !is_numeric($_POST['phone'])){
+        $message[] = 'SUCESS: False';
+        $message[]= 'Invalid format for phone number, Name is empty!';
+        echo json_encode($message);
+    }
+    else{
+        $phone = $_POST['phone'];
+        $name = ucfirst($_POST['name']);
+        $email = $_POST['email'];
+        $bio = $_POST['bio'];
+
+        addContact($name, $phone, $email, $bio);
+        echo json_encode(addContact($name, $phone, $email, $bio));
+        savePhonebook();
+    }
+        break;
+
+    //done
+    case 'delete':
+    if(empty($_POST['id'])){
+        $message[] = 'SUCESS: False';
+        $message[]= 'Contact ID is invalid';
+        echo json_encode($message); 
+    }
+
+    else{
+        $contact = $_POST['id'];
+        deleteContact($contact);
+        $message[] = 'SUCESS: True';
+        echo json_encode($message);
+        savePhonebook();
+    }  
+        break;
+   //done
+    case 'view':
+        echo json_encode(viewContact());
+        break;
+
+        
+    case 'update':
+        if((($_POST['id']=='') && ((empty($_POST['name']) || empty($_POST['phone']))|| empty($_POST['bio']) || empty($_POST['email'])))){
+            $message[] = 'SUCESS: False';
+            $message[]= 'id, name, phone is invalid';
+            echo json_encode($message);
+        }
+        else{
+            $id = $_POST['id'];
+            $newName = ucfirst($_POST['name']);
+            $newPhone = $_POST['phone'];
+            $email=$_POST['email'];
+            $bio=$_POST['bio'];
+            
+            echo json_encode(editContact($id, $newName, $newPhone, $email, $bio)); 
+            savePhonebook();
+        }
+        break;
+
+    case 'search':
+    $n = ucfirst($_POST['keyword']);
+            echo json_encode(searchContact($n));
+        break;
+    default:
+        echo 'There is an error, please check inputs';
+        break;
+
+    case 'deleteall';
+        echo Json_encode(deleteAll());
+}} 
+?>
